fe9ee09ea5de429e1ae723ac10c3d3a37ed802e7
Bloxorer <phantom16950@gmail.com>
2025-02-15 20:56:23 +0300

    added comments

--- src/main/java/com/pedropathing/util/AutoActions.java
+++ src/main/java/com/pedropathing/util/AutoActions.java
@@ -1,12 +1,17 @@
 package com.pedropathing.util;
 
 public abstract class AutoActions extends Thread{
+    // You must write here a number of path-chain with which you will use this action
     public int pathNumber = 0;
 
+    /**
+     *
+     * @param pathNumber with which pathchain will be used your action
+     */
     public AutoActions(int pathNumber) {
         this.pathNumber = pathNumber;
     }
-
+    // running a thread
     @Override
     public void run() {
         super.run();
