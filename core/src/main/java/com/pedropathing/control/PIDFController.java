aafb5f067e5c41690af144589182a81cc6cc3908
junkjunk123 <havitechrobo@gmail.com>
2025-09-01 10:38:32 -0700

    SquID and custom controller creation

--- core/src/main/java/com/pedropathing/control/PIDFController.java
+++ core/src/main/java/com/pedropathing/control/PIDFController.java
@@ -104,6 +104,11 @@ public class PIDFController implements Controller {
         previousUpdateTimeNano = System.nanoTime();
     }
 
+    @Override
+    public double run() {
+        return runPIDF();
+    }
+
     /**
      * This is used to set the target position if the PIDF is being run with current position and
      * target position inputs rather than error inputs.
@@ -139,6 +144,15 @@ public class PIDFController implements Controller {
      */
     public PIDFCoefficients getCoefficients() {
         return coefficients;
+    }
+
+    @Override
+    public void setCoefficients(Coefficients coefficients) {
+        if (coefficients instanceof PIDFCoefficients) {
+            setCoefficients((PIDFCoefficients) coefficients);
+        } else {
+            throw new IllegalArgumentException("Expected PIDFCoefficients");
+        }
     }
 
     /**
