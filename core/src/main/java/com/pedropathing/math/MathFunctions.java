a5824f0e0b7ebe697200b4325771a90602dcd96c
junkjunk123 <havitechrobo@gmail.com>
2025-11-22 13:39:03 -0800

    Patch FusionLocalizer timestamp interpolation for forward propagation

--- core/src/main/java/com/pedropathing/math/MathFunctions.java
+++ core/src/main/java/com/pedropathing/math/MathFunctions.java
@@ -193,5 +193,8 @@ public class MathFunctions {
         }
         
         return newMatrix;
+
+    public static double lerp(double a, double b, double t) {
+        return a + t * (b - a);
     }
 }
