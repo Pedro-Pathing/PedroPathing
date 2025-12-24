045892dbab55e72dc21e2cb13a945f77b4abc6d6
William <icaras84@gmail.com>
2025-12-24 01:21:20 -0800

    swapped exceptions for illegal argument exceptions

--- core/src/main/java/com/pedropathing/paths/PathBuilder.java
+++ core/src/main/java/com/pedropathing/paths/PathBuilder.java
@@ -117,12 +117,7 @@ public class PathBuilder {
     public PathBuilder curveThrough(Pose prevPoint, Pose startPoint, double tension, Pose... points){
         //guard against points being zero length (which means the curve doesn't have an end point)
         if (points.length == 0) {
-            try {
-                throw new Exception("Points array must contain at least one point to curve through.");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return this;
+            throw new IllegalArgumentException("Points array must contain at least one point to curve through.");
         }
         ArrayList<Pose> poses = new ArrayList<>();
 
