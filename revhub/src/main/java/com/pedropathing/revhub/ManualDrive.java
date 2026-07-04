b3574393c5526fa7131df2fd4955a9f3a0f5ce83
Baron <126834072+BaronClaps@users.noreply.github.com>
2026-07-04 15:31:35 -0500

    Field Centric Control in ManualDrive.java

--- revhub/src/main/java/com/pedropathing/revhub/ManualDrive.java
+++ revhub/src/main/java/com/pedropathing/revhub/ManualDrive.java
@@ -1,22 +1,18 @@
 package com.pedropathing.revhub;
 
 import com.pedropathing.drivetrain.DrivePowers;
-import com.pedropathing.follower.Follower;
-import com.qualcomm.robotcore.hardware.Gamepad;
+import com.pedropathing.math.Vector2D;
 
 public class ManualDrive {
-    private boolean fieldCentric = false;
-    private double offsetHeading = 0;
-    private boolean headingLock = false;
 
-    private final Follower follower;
-
-    public ManualDrive(Follower follower) {
-        this.follower = follower;
+    /** Takes in robotCentric drive powers and uses the currentHeading to rotate them to fieldCentric drive powers. */
+    public static DrivePowers fieldCentric(DrivePowers powers, double currentHeading) {
+        return fieldCentric(powers, currentHeading, 0.0);
     }
 
-    //TODO: Write robot-centric and field-centric code, offset vector, heading lock, etc
-    public DrivePowers computeDrive(Gamepad gamepad) {
-        return null;
+    /** Takes in robotCentric drive powers and uses the currentHeading to rotate them to fieldCentric drive powers with an offset heading. */
+    public static DrivePowers fieldCentric(DrivePowers powers, double currentHeading, double offset) {
+        Vector2D fieldRelative = Vector2D.cartesian(powers.forward(), powers.strafe()).rotate(currentHeading + offset);
+        return new DrivePowers(fieldRelative.x(), fieldRelative.y(), powers.turn());
     }
 }
