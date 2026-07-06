53b00776508fb9417c21b3931161df8a4314cac3
junkjunk123 <havitechrobo@gmail.com>
2026-07-05 20:37:11 -0700

    heading lock

--- revhub/src/main/java/com/pedropathing/revhub/ManualDrive.java
+++ revhub/src/main/java/com/pedropathing/revhub/ManualDrive.java
@@ -1,6 +1,11 @@
 package com.pedropathing.revhub;
 
+import static com.pedropathing.utils.Angle.normalizeSigned;
+
+import com.pedropathing.controllers.PIDController;
 import com.pedropathing.drivetrain.DrivePowers;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
 import com.pedropathing.math.Vector2D;
 
 public class ManualDrive {
@@ -23,5 +28,11 @@ public class ManualDrive {
     /** Takes in robotCentric drive powers and uses the currentHeading to rotate them to fieldCentric drive powers with an offset heading. */
     public static DrivePowers fieldCentric(double forward, double lateral, double turn, double currentHeading, double offsetHeading) {
         return fieldCentric(new DrivePowers(forward, lateral, turn), currentHeading, offsetHeading);
+    }
+
+    public static DrivePowers headingLock(Follower follower, PIDController headingPID, DrivePowers powers, double targetHeading) {
+        double headingError = normalizeSigned(targetHeading - follower.pose().heading());
+        double power = headingPID.calculate(targetHeading, headingError, follower.twist().omega());
+        return new DrivePowers(powers.forward(), powers.strafe(), power);
     }
 }
