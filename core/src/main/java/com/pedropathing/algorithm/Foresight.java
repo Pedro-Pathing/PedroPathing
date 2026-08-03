3d6cd2fdbbe680331a2d373438e763bbd81f1c19
junkjunk123 <havitechrobo@gmail.com>
2026-08-03 14:57:41 -0700

    patch heading ff

--- core/src/main/java/com/pedropathing/algorithm/Foresight.java
+++ core/src/main/java/com/pedropathing/algorithm/Foresight.java
@@ -281,7 +281,7 @@ public class Foresight implements Algorithm {
         List<Pair<Vector2D, Boolean>> vectors;
 
         //allocate heading power before + after drive
-        headingFeedforward = headingPower * config.headingDriveRatio.get();
+        headingFeedforward += headingPower * config.headingDriveRatio.get();
         headingPower *= (1 - config.headingDriveRatio.get());
 
         if (translationalPriority && headingPriority) {
