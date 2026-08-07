79bffbb398f238d4c829060407fb80157459bb0d
junkjunk123 <havitechrobo@gmail.com>
2026-08-07 11:35:17 -0700

    patch swerve impl

--- core/src/main/java/com/pedropathing/drivetrain/Drivetrain.java
+++ core/src/main/java/com/pedropathing/drivetrain/Drivetrain.java
@@ -6,10 +6,11 @@ package com.pedropathing.drivetrain;
 
 import com.pedropathing.localization.MotionState;
 import com.pedropathing.math.Vector2D;
+import com.pedropathing.utils.Pair;
+import com.pedropathing.utils.Utils;
 
 public interface Drivetrain {
     void drive(DrivePowers powers, boolean manual);
-    double[] computeWheelPowers(DrivePowers powers);
     double maxScaling(DrivePowers current,
                              DrivePowers delta);
     void stop();
