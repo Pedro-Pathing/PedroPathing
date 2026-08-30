fb2ef6eef8a60ca4555c6e3ae08967acb9e98744
junkjunk123 <havitechrobo@gmail.com>
2026-08-30 11:23:30 -0700

    ForesightV3 -> Foresight

--- core/src/main/java/com/pedropathing/algorithm/Foresight.java
+++ core/src/main/java/com/pedropathing/algorithm/Foresight.java
@@ -33,6 +33,8 @@ public class Foresight implements Algorithm {
     private double headingError, translationalError, targetVelocity;
     private boolean busy = false;
     private final Vector2D naturalDeceleration;
+    private PathTracker tracker;
+    private MotionState currentState;
 
     public Foresight(ForesightConfig config) {
         this.config = config;
@@ -44,7 +46,9 @@ public class Foresight implements Algorithm {
     @Override
     public DrivePowers calculatePath(
             Drivetrain drivetrain, PathTracker pathTracker, MotionState state, double deltaTime) {
-        closestT = pathTracker.current().curve.closestT(state.pose().toVector2D(), closestT);
+        tracker = pathTracker;
+        currentState = state;
+        closestT = pathTracker.current().curve.closestParameter(state.pose().toVector2D(), closestT);
 
         if (testParametric()) { // End Constraint
             closestT = 1.0;
@@ -78,7 +82,7 @@ public class Foresight implements Algorithm {
         Curve curve = pathTracker.current().curve;
         Pose projectedPose = state.pose()
                 .plus(getBrakeDisplacement(state.twist(), state.pose().heading()));
-        projectedClosestT = curve.closestT(projectedPose.toVector2D(), projectedClosestT);
+        projectedClosestT = curve.closestParameter(projectedPose.toVector2D(), projectedClosestT);
         double targetHeading = closestPose.heading();
         double projectedTargetHeading = pathTracker.current().heading(projectedClosestT);
         Vector2D projectedTangent = curve.tangent(projectedClosestT);
@@ -126,7 +130,6 @@ public class Foresight implements Algorithm {
                 isBraking,
                 velocityToBrakeInTime,
                 deltaTime,
-                projectedRemainingDist,
                 angleToTangent,
                 tangentialSpeed,
                 state,
@@ -155,7 +158,7 @@ public class Foresight implements Algorithm {
             }
         } else {
             if (((Math.abs(headingError) > 2 * config.headingDeviationTolerance.get())
-                            || (Math.abs(translationalError) > 2 * config.translationalDeviationTolerance.get()))
+                    || (Math.abs(translationalError) > 2 * config.translationalDeviationTolerance.get()))
                     && config.cosineScale.get())
                 drive = drive.times(allocator.getDriveScalar(translationalError, headingError));
         }
@@ -175,6 +178,10 @@ public class Foresight implements Algorithm {
     @Override
     public DrivePowers calculateHold(
             Drivetrain drivetrain, Pose target, MotionState state, boolean useScaling, double deltaTime) {
+        tracker = null;
+        currentState = state;
+        closestT = 1.0;
+
         if (resetTimer) {
             timer.reset();
             resetTimer = false;
@@ -223,8 +230,8 @@ public class Foresight implements Algorithm {
                 linearTwist.hadamardProduct(linearTwist.abs()).transform(config.quadraticBrakeCoefficients.get());
         Vector2D linear = linearTwist.transform(config.linearBrakeCoefficients.get());
         double headingDisp = twist.omega
-                        * Math.abs(twist.omega)
-                        * config.headingBrakeCoefficients.get().y()
+                * Math.abs(twist.omega)
+                * config.headingBrakeCoefficients.get().y()
                 + twist.omega * config.headingBrakeCoefficients.get().x();
         Vector2D bodyDisp = quadratic.plus(linear);
         Pose worldPose = new Pose(0, 0, heading).exp(new Twist(bodyDisp.x(), bodyDisp.y(), headingDisp));
@@ -236,8 +243,8 @@ public class Foresight implements Algorithm {
         Vector2D bodyDisp =
                 linearTwist.hadamardProduct(linearTwist.abs()).times(1.0 / 2.0).elementDivision(naturalDeceleration);
         double headingDisp = twist.omega
-                        * Math.abs(twist.omega)
-                        * config.headingBrakeCoefficients.get().y()
+                * Math.abs(twist.omega)
+                * config.headingBrakeCoefficients.get().y()
                 + twist.omega * config.headingBrakeCoefficients.get().x();
         Pose worldPose = new Pose(0, 0, heading).exp(new Twist(bodyDisp.x(), bodyDisp.y(), headingDisp));
         return new Pose(worldPose.x(), worldPose.y(), headingDisp);
@@ -269,7 +276,6 @@ public class Foresight implements Algorithm {
             boolean isBraking,
             double profiledTargetVelocity,
             double deltaTime,
-            double remainingDistance,
             double angleToTangent,
             double tangentialVel,
             MotionState state,
@@ -307,7 +313,7 @@ public class Foresight implements Algorithm {
         if (maxDecelerationConstraint != ForesightConfig.Constraint.NONE) {
             Pose projected = state.pose()
                     .plus(getCoastDisplacement(state.twist(), state.pose().heading()));
-            double projectedT = curve.closestT(projected.toVector2D(), coastClosestT);
+            double projectedT = curve.closestParameter(projected.toVector2D(), coastClosestT);
             Vector2D projectedTangent = curve.tangent(projectedT);
             double projectedRemainingDist = curve.remainingDistance(projectedT);
             if (projectedRemainingDist <= 0.01)
@@ -388,8 +394,9 @@ public class Foresight implements Algorithm {
     }
 
     @Override
-    public double closestT() {
-        return closestT;
+    public double completion() {
+        if (tracker == null) return 1.0;
+        return tracker.path().curve.pathCompletion(closestT);
     }
 
     @Override
@@ -423,8 +430,8 @@ public class Foresight implements Algorithm {
     }
 
     @Override
-    public boolean atParametricEnd(double t) {
-        return config.parametricTConstraint.get() < t;
+    public boolean atParametricEnd() {
+        return testParametric();
     }
 
     @Override
@@ -435,6 +442,7 @@ public class Foresight implements Algorithm {
         closestT = 0.0;
         projectedClosestT = 0.0;
         coastClosestT = 0.0;
+        tracker = null;
     }
 
     @Override
